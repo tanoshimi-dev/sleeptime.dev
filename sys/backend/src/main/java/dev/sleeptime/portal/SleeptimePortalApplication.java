d835bbef6ce9ee4e9ab184b1de83ba61f6df86d3
tanoshimi-dev <keijimitaki@gmail.com>
2026-03-09 10:53:28 +0900

    [backend]phase1 1.1

---
+++ sys/backend/src/main/java/dev/sleeptime/portal/SleeptimePortalApplication.java
@@ -0,0 +1,12 @@
+package dev.sleeptime.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleeptimePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleeptimePortalApplication.class, args);
+    }
+}
---
+++ sys/backend/src/test/java/dev/sleeptime/portal/SleeptimePortalApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.sleeptime.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class SleeptimePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
